0f3a2331c9e73394bc1ba88e5f24ea4ea58cd523
keerthana020408 <192521062.simats@saveetha.com>
2026-08-20 10:54:24 +0530

    Add files via upload

---
+++ CO1-EXPERIMMENT-8/script.js
@@ -0,0 +1,24 @@
+function welcomeMessage() {
+
+    alert(
+        "✨ Welcome to Keerthana's Web Technology Lab! ✨\n\n" +
+        "Name: Keerthana\n" +
+        "Course: B.Tech Information Technology\n" +
+        "Year: II Year\n" +
+        "College: SIMATS Engineering"
+    );
+
+}
+
+
+function iframeMessage() {
+
+    alert(
+        "🌐 IFRAME ELEMENT\n\n" +
+        "An iframe is an HTML element used to embed " +
+        "external content inside the current webpage.\n\n" +
+        "In this experiment, an OpenStreetMap map " +
+        "is embedded using the <iframe> element."
+    );
+
+}
